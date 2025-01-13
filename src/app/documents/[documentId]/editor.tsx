f7a447dc55ede5ef485368e687fca90b9d9de1e2
Ricardo Bassete <ricardogbassete@gmail.com>
2025-01-13 20:30:43 -0300

    add tiptap table extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,6 +4,10 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 
 export const Editor = () => {
   const editor = useEditor({
@@ -13,7 +17,7 @@ export const Editor = () => {
         class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
-    extensions: [StarterKit, TaskList, TaskItem.configure({ nested: true })],
+    extensions: [StarterKit, TaskList, TaskItem.configure({ nested: true }), Table, TableCell, TableHeader, TableRow],
     content: '<p>Hello World! 🌎️</p>',
   })
 
