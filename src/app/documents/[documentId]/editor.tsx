72441080f62e90c314aeb26f0bc9fdda85bbf5e8
Ricardo Bassete <ricardogbassete@gmail.com>
2025-01-13 20:15:40 -0300

    add tiptap task list

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,7 +13,7 @@ export const Editor = () => {
         class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
-    extensions: [StarterKit],
+    extensions: [StarterKit, TaskList, TaskItem.configure({ nested: true })],
     content: '<p>Hello World! 🌎️</p>',
   })
 
