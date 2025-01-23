02fe2af4df61e702af808e0b75ddc02fbcd55812
Ricardo Bassete <ricardogbassete@gmail.com>
2025-01-23 15:30:27 -0300

    Add text color and highlight

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -13,6 +13,8 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
 
 import { useEditorStore } from '@/store/use-editor-store'
 
@@ -53,6 +55,8 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Color,
+      Highlight.configure({ multicolor: true }),
       TaskList,
       TaskItem.configure({ nested: true }),
       Underline,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils'
 import {
   BoldIcon,
   ChevronDownIcon,
+  HighlighterIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -17,6 +18,7 @@ import {
 } from 'lucide-react'
 
 import { type Level } from '@tiptap/extension-heading'
+import { type ColorResult, SketchPicker } from 'react-color'
 
 import { useEditorStore } from '@/store/use-editor-store'
 import { Separator } from '@/components/ui/separator'
@@ -146,6 +148,51 @@ const HeadingLevelButton = () => {
   )
 }
 
+const TextColorButton = () => {
+  const { editor } = useEditorStore()
+  const value = editor?.getAttributes('textStyle').color || '#000000'
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setColor(color.hex).run()
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="text-xs">A</span>
+          <div className="h-0.5 w-full" style={{ backgroundColor: value }} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+const HighightColorButton = () => {
+  const { editor } = useEditorStore()
+  const value = editor?.getAttributes('highlight').color || '#FFFFFF'
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setHighlight({ color: color.hex }).run()
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <HighlighterIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 export const Toolbar = () => {
   const { editor } = useEditorStore()
 
@@ -244,8 +291,8 @@ export const Toolbar = () => {
       {sections[1].map(item => (
         <ToolbarButton key={item.label} {...item} />
       ))}
-      {/* TODO: Text Color */}
-      {/* TODO: Highlight Color */}
+      <TextColorButton />
+      <HighightColorButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Link */}
       {/* TODO: Image */}
